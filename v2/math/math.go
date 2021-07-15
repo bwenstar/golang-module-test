166c8db94c10a7f19bd79e73cb08b4423d470b2b
brendanta <brendan.ta@jacques.com.au>
2021-07-15 11:32:15 +1000

    Add math functions

---
+++ v2/math/math.go
@@ -0,0 +1,17 @@
+package bta_math
+
+func Add(a, b int) int {
+    return a + b
+}
+
+func Sub(a, b int) int {
+    return a - b
+}
+
+func Multi(a, b int) int {
+    return a * b
+}
+
+func Divide(a, b int) int {
+    return a / b
+}
