b45cf2a9e375293e0d72efe1ba1565ba20282415
brendanta <brendan.ta@jacques.com.au>
2021-07-15 10:34:21 +1000

    Add additional func, goodbye.

--- v2/hello.go
+++ v2/hello.go
@@ -6,3 +6,8 @@ func Hello(name string) string {
     message := fmt.Sprintf("Hello, %v! (v2)", name)
     return message
 }
+
+func Goodbye(name string) string {
+    message := fmt.Sprintf("Goodbye, %v! (v2)", name)
+    return message
+}
